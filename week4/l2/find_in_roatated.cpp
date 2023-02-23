2ba6318f95ab44c1141575361e1eed540e1bef07
umairmohd8 <mohammed08@protonmail.com>
2023-02-23 17:17:10 +0530

    rotated- returning -1

--- week4/l2/find_in_roatated.cpp
+++ week4/l2/find_in_roatated.cpp
@@ -3,7 +3,7 @@
 // eg: 4 5 6 7 0 1 2 3 
 // [7 1 2 3 4 5 6] 
 
-
+//{7,8,9,1,2,3,4,5,6}
 // find pivot first then find the element 
 
 #include <vector>
@@ -13,9 +13,9 @@ using namespace std;
 
 int find_pivot(vector<int> arr, int start, int end) {
     int mid = start + (end - start) / 2;
-    if(arr[mid] > arr[mid+1])
+    if( mid+1 < arr.size() && arr[mid] > arr[mid+1])
         return mid;
-    else if(arr[mid -1] > arr[mid])
+    else if(mid-1 > 0 && arr[mid -1] > arr[mid])
         return mid-1;
     else if(arr[mid] < arr[start])
         find_pivot(arr, start, mid-1);
@@ -37,6 +37,7 @@ int find_element(vector<int> arr, int target){
             e = mid-1;
         mid =  s+ (e-s)/2;
     }
+    return -1;
 }
 
 int main(){
